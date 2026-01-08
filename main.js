92e88ba6d4d03426e3c6aee256ef08c66ebda075
Lia <aliyahrenomeron7@gmail.com>
2026-01-08 16:47:30 -0500

    added the script for the js file and adjusted the dimensions of the window

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 600,
+        height: 700
     })
 
     win.loadFile('index.html')
