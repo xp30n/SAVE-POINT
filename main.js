22be7350fb54182fc1b6429344cc27395c6d41ca
Lia <aliyahrenomeron7@gmail.com>
2026-01-08 22:57:31 -0500

    temporarily removed the transparent title bar and added an id to the start button

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 600,
         height: 700,
-        frame: false
+        // frame: false
     })
 
     win.loadFile('index.html')
